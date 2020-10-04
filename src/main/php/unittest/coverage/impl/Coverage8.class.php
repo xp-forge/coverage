7324454e4df3130821a8679faba901891b5fa3f9
Timm Friebe <thekid@thekid.de>
2020-10-04 11:25:44 +0200

    QA: Add api docs

--- src/main/php/unittest/coverage/impl/Coverage8.class.php
+++ src/main/php/unittest/coverage/impl/Coverage8.class.php
@@ -5,27 +5,51 @@ use SebastianBergmann\CodeCoverage\{CodeCoverage, Filter};
 class Coverage8 implements Implementation {
   private $filter, $backing;
 
+  /** Creates a new coverage implementation for php-code-coverage < 9.0 */
   public function __construct() {
     $this->filter= new Filter();
     $this->backing= new CodeCoverage(null, $this->filter);
   }
 
+  /**
+   * Adds a folder as target
+   *
+   * @param  string $folder
+   * @return void
+   */
   public function target($folder) {
     $this->filter->addDirectoryToWhitelist($folder);
   }
 
+  /**
+   * Returns whether targets are present
+   *
+   * @return bool
+   */
   public function targetsPresent() {
     return $this->filter->hasWhitelist();
   }
 
+  /**
+   * Starts coverage
+   *
+   * @param  string $name
+   * @return void
+   */
   public function start($name) {
     $this->backing->start($name);  // @codeCoverageIgnore
   }
 
+  /**
+   * Stops coverage
+   *
+   * @return void
+   */
   public function stop() {
     $this->backing->stop();
   }
 
+  /** @return unittest.coerage.impl.Report */
   public function report() {
     $report= $this->backing->getReport();
     return new Report(
--- src/main/php/unittest/coverage/impl/Coverage9.class.php
+++ src/main/php/unittest/coverage/impl/Coverage9.class.php
@@ -6,27 +6,51 @@ use SebastianBergmann\CodeCoverage\{CodeCoverage, Filter};
 class Coverage9 implements Implementation {
   private $filter, $backing;
 
+  /** Creates a new coverage implementation for php-code-coverage >= 9.0 */
   public function __construct() {
     $this->filter= new Filter();
     $this->backing= new CodeCoverage((new Selector())->forLineCoverage($this->filter), $this->filter);
   }
 
+  /**
+   * Adds a folder as target
+   *
+   * @param  string $folder
+   * @return void
+   */
   public function target($folder) {
     $this->filter->includeDirectory($folder);
   }
 
+  /**
+   * Returns whether targets are present
+   *
+   * @return bool
+   */
   public function targetsPresent() {
     return !$this->filter->isEmpty();
   }
 
+  /**
+   * Starts coverage
+   *
+   * @param  string $name
+   * @return void
+   */
   public function start($name) {
     $this->backing->start($name);  // @codeCoverageIgnore
   }
 
+  /**
+   * Stops coverage
+   *
+   * @return void
+   */
   public function stop() {
     $this->backing->stop();
   }
 
+  /** @return unittest.coerage.impl.Report */
   public function report() {
     $report= $this->backing->getReport();
     return new Report(
