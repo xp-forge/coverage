6e7f2922fcf44d5dfcf749d08abb7b2300a5a8c0
Timm Friebe <thekid@thekid.de>
2020-10-04 12:16:38 +0200

    Add workaround for fatal error in php-code-coverage library
    See sebastianbergmann/php-code-coverage#820

--- src/main/php/unittest/coverage/impl/Coverage9.class.php
+++ src/main/php/unittest/coverage/impl/Coverage9.class.php
@@ -1,5 +1,6 @@
 <?php namespace unittest\coverage\impl;
 
+use PHPUnit\Runner\BaseTestRunner;
 use SebastianBergmann\CodeCoverage\Driver\Selector;
 use SebastianBergmann\CodeCoverage\Report\Clover;
 use SebastianBergmann\CodeCoverage\Report\Html\Facade;
@@ -7,6 +8,24 @@ use SebastianBergmann\CodeCoverage\{CodeCoverage, Filter};
 
 class Coverage9 implements Implementation {
   private $filter, $backing;
+
+  static function __static() {
+
+    // Workaround for fatal error in php-code-coverage when PHPUnit is not loaded
+    // See https://github.com/sebastianbergmann/php-code-coverage/issues/820
+    if (!class_exists(BaseTestRunner::class)) {
+      eval('namespace PHPUnit\Runner; class BaseTestRunner {
+        const STATUS_UNKNOWN    = -1;
+        const STATUS_PASSED     = 0;
+        const STATUS_SKIPPED    = 1;
+        const STATUS_INCOMPLETE = 2;
+        const STATUS_FAILURE    = 3;
+        const STATUS_ERROR      = 4;
+        const STATUS_RISKY      = 5;
+        const STATUS_WARNING    = 6;
+      }');
+    }
+  }
 
   /** Creates a new coverage implementation for php-code-coverage >= 9.0 */
   public function __construct() {
