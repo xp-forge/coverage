4f93e8db767d2b4e922c56ed16796fc32562eaa3
Timm Friebe <thekid@thekid.de>
2020-10-31 10:58:25 +0100

    Remove workaround for sebastianbergmann/php-code-coverage#820

--- src/main/php/unittest/coverage/impl/Coverage9.class.php
+++ src/main/php/unittest/coverage/impl/Coverage9.class.php
@@ -7,25 +7,6 @@ use SebastianBergmann\CodeCoverage\Driver\Selector;
 /** Coverage implementation for php-code-coverage >= 9.0 */
 class Coverage9 extends Implementation {
 
-  /** @codeCoverageIgnore */
-  static function __static() {
-
-    // Workaround for fatal error in php-code-coverage when PHPUnit is not loaded
-    // See https://github.com/sebastianbergmann/php-code-coverage/issues/820
-    if (!class_exists(BaseTestRunner::class)) {
-      eval('namespace PHPUnit\Runner; class BaseTestRunner {
-        const STATUS_UNKNOWN    = -1;
-        const STATUS_PASSED     = 0;
-        const STATUS_SKIPPED    = 1;
-        const STATUS_INCOMPLETE = 2;
-        const STATUS_FAILURE    = 3;
-        const STATUS_ERROR      = 4;
-        const STATUS_RISKY      = 5;
-        const STATUS_WARNING    = 6;
-      }');
-    }
-  }
-
   /** Creates a new backing instance */
   protected function newInstance(): CodeCoverage {
     return new CodeCoverage((new Selector())->forLineCoverage($this->filter), $this->filter);
